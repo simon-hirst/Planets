0e1bae6ff59393a9fa9c524f9c82dcd9c9643077
Simon Hirst <simonhirst@protonmail.com>
2020-01-25 19:59:38 +0000

    Creating initial react files

---
+++ Planets.Front/planets/src/PlanetContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class PlanetContainer extends React.Component {
+  render () {
+    return <div className='message-box'>
+      Hello retard
+    </div>
+  }
+}
+
+export default PlanetContainer;
